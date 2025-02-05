6d32cae57fd2ae65f56f66108debc39973136887
Alex Gutierrez <agutierrezlavin@gmail.com>
2025-02-05 23:13:45 +0100

    Added featured Funcionality

--- src/app/components/ToolList.tsx
+++ src/app/components/ToolList.tsx
@@ -2,10 +2,52 @@
 
 import CategoryFilters from './CategoryFilters'
 import ToolGallery from './ToolGallery'
+import { Tool } from '../entities/Tools'
 import { useState } from 'react'
+
+const DEFAULT_NUMBER_OF_TOOLS: number = 30
+
+function getFilteredToolsByNumber(
+  data: Tool[],
+  number: number = DEFAULT_NUMBER_OF_TOOLS,
+) {
+  return data.slice(0, number)
+}
 
 export default function ToolList({ tools }: { tools: any }) {
   const [filteredResults, setFilteredResults] = useState<string>('')
+
+  const filteredDevelopmentToolsDataByNumber = getFilteredToolsByNumber(
+    tools.development,
+    DEFAULT_NUMBER_OF_TOOLS,
+  )
+  const filteredDesignToolsDataByNumber = getFilteredToolsByNumber(
+    tools.design,
+    DEFAULT_NUMBER_OF_TOOLS,
+  )
+
+  const filteredProductivityToolsDataByNumber = getFilteredToolsByNumber(
+    tools.productivity,
+    DEFAULT_NUMBER_OF_TOOLS,
+  )
+
+  const featuredDevelopmentTools = tools.development
+    .filter((tool: any) => tool.featured)
+    .map((tool: any) => tool)
+
+  const featuredDesignTools = tools.design
+    .filter((tool: any) => tool.featured)
+    .map((tool: any) => tool)
+
+  const featuredProducityivTools = tools.productivity
+    .filter((tool: any) => tool.featured)
+    .map((tool: any) => tool)
+
+  const featuredTools = [
+    ...featuredDevelopmentTools,
+    ...featuredDesignTools,
+    ...featuredProducityivTools,
+  ]
 
   const handleResultsFilter = (filter: string) => {
     setFilteredResults(filter)
@@ -13,6 +55,11 @@ export default function ToolList({ tools }: { tools: any }) {
 
   return (
     <>
+      <section className="w-full grid grid-cols-1 gap-8 sm:grid-cols-3">
+        <h1 className="col-span-full text-2xl font-bold">Featured</h1>
+        <ToolGallery tools={featuredTools} />
+      </section>
+
       <section>
         <CategoryFilters
           filteredValue={filteredResults}
@@ -22,24 +69,20 @@ export default function ToolList({ tools }: { tools: any }) {
 
       {(filteredResults === '' || filteredResults === 'development') && (
         <section className="w-full grid grid-cols-1 gap-8 sm:grid-cols-3">
-          <h1 className="col-span-full text-2xl font-bold">
-            Development Tools
-          </h1>
+          <h1 className="col-span-full text-2xl font-bold">Development</h1>
           <ToolGallery tools={tools.development} />
         </section>
       )}
 
       {(filteredResults === '' || filteredResults === 'design') && (
         <section className="w-full grid grid-cols-1 gap-8 sm:grid-cols-3">
-          <h1 className="col-span-full text-2xl font-bold">Design Tools</h1>
+          <h1 className="col-span-full text-2xl font-bold">Design</h1>
           <ToolGallery tools={tools.design} />
         </section>
       )}
       {(filteredResults === '' || filteredResults === 'productivity') && (
         <section className="w-full grid grid-cols-1 gap-8 sm:grid-cols-3">
-          <h1 className="col-span-full text-2xl font-bold">
-            Productivity Tools
-          </h1>
+          <h1 className="col-span-full text-2xl font-bold">Productivity</h1>
           <ToolGallery tools={tools.productivity} />
         </section>
       )}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
-import ToolGallery from './components/ToolGallery'
 import Sidebar from './components/Sidebar'
+import ToolList from './components/ToolList'
 import { promises as fs } from 'fs'
 import { Tool } from './entities/Tools'
-import { Button, HStack } from '@chakra-ui/react'
-import CategoryFilters from './components/CategoryFilters'
-import { useState } from 'react'
-import ToolList from './components/ToolList'
-
-const DEFAULT_NUMBER_OF_TOOLS: number = 30
-
-function getFilteredToolsByNumber(
-  data: Tool[],
-  number: number = DEFAULT_NUMBER_OF_TOOLS,
-) {
-  return data.slice(0, number)
-}
 
 export default async function Home() {
   const dataDevelopmentTools: string = await fs.readFile(
@@ -24,37 +11,18 @@ export default async function Home() {
     'utf8',
   )
   const parsedDevelopmentTools: Tool[] = JSON.parse(dataDevelopmentTools)
-  const filteredDevelopmentToolsDataByNumber = getFilteredToolsByNumber(
-    parsedDevelopmentTools,
-    DEFAULT_NUMBER_OF_TOOLS,
-  )
 
   const dataDesignTools = await fs.readFile(
     process.cwd() + '/src/app/data/design.json',
     'utf8',
   )
   const parsedDesignTools: Tool[] = JSON.parse(dataDesignTools)
-  const filteredDesignToolsDataByNumber = getFilteredToolsByNumber(
-    parsedDesignTools,
-    DEFAULT_NUMBER_OF_TOOLS,
-  )
 
   const dataProductivityTools = await fs.readFile(
     process.cwd() + '/src/app/data/productivity.json',
     'utf8',
   )
   const parsedProductivityTools: Tool[] = JSON.parse(dataProductivityTools)
-  const filteredProductivityToolsDataByNumber = getFilteredToolsByNumber(
-    parsedProductivityTools,
-    DEFAULT_NUMBER_OF_TOOLS,
-  )
-
-  function setResultsByCategoriy() {}
-
-  /*   const handleClick = (filter: string) => {
-    console.log(filter)
-  }
- */
 
   const tools = {
     development: parsedDevelopmentTools,
